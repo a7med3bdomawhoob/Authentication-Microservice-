786e9b7103e71320a764cdadf12463dfcdfe8c45
Engaabdo971 <Engaabdo971@gmail.com>
2024-11-19 10:56:10 +0200

    Authentication

---
+++ DAL/Database/Dbcontext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Database
+{
+    public class Dbcontext : IdentityDbContext<AppUser>
+    {
+        public Dbcontext(DbContextOptions<Dbcontext> options)
+       : base(options)
+        {
+        }
+
+    }
+}
---
+++ DAL/Entities/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Entities
+{
+    public class AppUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public AppUser(string userName, string email) : base(userName)
+        {
+            Email = email;
+            FirstName = string.Empty; // Default value
+            LastName = string.Empty;  // Default value
+        }
+
+
+    }
+}
